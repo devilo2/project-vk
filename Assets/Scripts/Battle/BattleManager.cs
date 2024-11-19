cdc75d2c72c4a514f534cef1c61be8bd8bfd265a
kinjae00 <kinjae0227@naver.com>
2024-11-19 15:41:13 +0900

    temp

--- Assets/Scripts/Battle/BattleManager.cs
+++ Assets/Scripts/Battle/BattleManager.cs
@@ -2,7 +2,9 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics.Tracing;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class BattleManager : MonoBehaviour
 {
@@ -12,10 +14,11 @@ public class BattleManager : MonoBehaviour
 
     BattleStatus curBattleStatus = BattleStatus.None;
     PlayerTurnStatus curPlayerTurnStatus = PlayerTurnStatus.Idle;
+    EnemyTurnStatus curEnemyturnStatus = EnemyTurnStatus.EnemyTurn;
 
     int skillNum = 0;
 
-    Enemy[] enemies; 
+    Enemy[] enemies = new Enemy[3]; 
     int enemyNum = 0;
     int enemyMax = 0;
 
@@ -24,6 +27,10 @@ public class BattleManager : MonoBehaviour
     {
         energyAmplification = true; 
     }
+
+
+    bool enterKey = false;
+    bool escKey = false;
 
     enum BattleStatus
     {
@@ -37,7 +44,14 @@ public class BattleManager : MonoBehaviour
     {
         Idle,
         EnemySelect,
-        SkillUse
+        SkillUse,
+        End
+    }
+
+    enum EnemyTurnStatus
+    {
+        EnemyTurn,
+        End
     }
 
 
@@ -46,18 +60,44 @@ public class BattleManager : MonoBehaviour
     {
         playerData = GameObject.Find("PlayerManager").GetComponent<PlayerData>();
         curBattleStatus = BattleStatus.PlotSelect;
+
+        //ï¿½×½ï¿½Æ® ï¿½Úµï¿½
+        Enemy test_enemy = new Enemy("ï¿½×½ï¿½Æ® ï¿½ï¿½");
+        enemies[0] = test_enemy;
     }
 
     // Update is called once per frame
     void Update()
     {
-        switch(curBattleStatus)
+        //Å°ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½Î½ÄµÇ¹Ç·ï¿½ ifï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ Å° ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ falseï¿½ï¿½ ï¿½Ù²ï¿½ï¿½Ø´ï¿½.
+        enterKey = Input.GetKeyDown(KeyCode.Return); 
+        escKey = Input.GetKeyDown(KeyCode.Escape);
+
+        switch (curBattleStatus)
         {
             case BattleStatus.PlotSelect:
-                if (Input.GetKeyDown(KeyCode.Return))
+                if (enterKey)
                 {
                     curBattleStatus = BattleStatus.PlayerTurn;
-                    Debug.Log($"BattleManager: ÇöÀç ÇÃ·Ô:{playerPlot}");
+                    skillNum = 0;
+                    curPlayerTurnStatus = PlayerTurnStatus.Idle;
+                    Debug.Log($"BattleManager: ï¿½ï¿½ï¿½ï¿½ ï¿½Ã·ï¿½:{playerPlot}");
+                    Debug.Log("ï¿½Ã·ï¿½ï¿½Ì¾ï¿½ï¿½ï¿½");
+                    enterKey = false;
+                }
+                break;
+            case BattleStatus.PlayerTurn:
+                if (curPlayerTurnStatus == PlayerTurnStatus.End)
+                {
+                    curBattleStatus = BattleStatus.EnemyTurn;
+                    Debug.Log("ï¿½ï¿½ï¿½ï¿½");
+                }
+                break;
+            case BattleStatus.EnemyTurn:
+                if(curEnemyturnStatus == EnemyTurnStatus.End)
+                {
+                    curBattleStatus = BattleStatus.PlayerTurn;
+                    Debug.Log("ï¿½Ã·ï¿½ï¿½Ì¾ï¿½ï¿½ï¿½");
                 }
                 break;
         }
@@ -94,22 +134,38 @@ public class BattleManager : MonoBehaviour
 
     private void PlayerTurn()
     {
-        switch(curPlayerTurnStatus)
+        
+
+        switch (curPlayerTurnStatus)
         {
             case PlayerTurnStatus.Idle:
-                if (Input.GetKeyDown(KeyCode.Return))
+                if (enterKey)
+                {
                     curPlayerTurnStatus = PlayerTurnStatus.EnemySelect;
+                    Debug.Log("EnemySelectï¿½ï¿½ ï¿½Ìµï¿½");
+                    enterKey = false;
+                }
+
                 break;
 
             case PlayerTurnStatus.EnemySelect:
-                if (Input.GetKeyDown(KeyCode.Return))
+                if (escKey)
+                {
                     curPlayerTurnStatus = PlayerTurnStatus.Idle;
-                if (Input.GetKeyDown(KeyCode.Escape))
+                    Debug.Log("Idleï¿½ï¿½ ï¿½Ìµï¿½");
+                    escKey = false;
+                }
+                if (enterKey)
+                {
                     if (IsSkillCostUnderPlot())
+                    {
                         curPlayerTurnStatus = PlayerTurnStatus.SkillUse;
+                        Debug.Log("SkillUseï¿½ï¿½ ï¿½Ìµï¿½");
+                    }
                     else
-                        Debug.Log("BattleManager: ÄÚ½ºÆ® ºÎÁ·");
-
+                        Debug.Log("BattleManager: ï¿½Ú½ï¿½Æ® ï¿½ï¿½ï¿½ï¿½");
+                    enterKey = false;
+                }
                 break;
         }
 
@@ -146,6 +202,7 @@ public class BattleManager : MonoBehaviour
                 break;
             case PlayerTurnStatus.SkillUse:
                 playerData.getSkill(skillNum).UseSkill(enemies[enemyNum]);
+                curPlayerTurnStatus = PlayerTurnStatus.End;
                 break;
 
         }
@@ -158,7 +215,7 @@ public class BattleManager : MonoBehaviour
 
 
 
-    //½ºÅ³ÀÌ ÄÚ½ºÆ® ÀÌÇÏÀÎÁö Ã¼Å©
+    //ï¿½ï¿½Å³ï¿½ï¿½ ï¿½Ú½ï¿½Æ® ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ Ã¼Å©
     private Boolean IsSkillCostUnderPlot()
     {
         int maxCost = playerPlot;
--- Assets/Scripts/Battle/Enemy.cs
+++ Assets/Scripts/Battle/Enemy.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 public class Enemy
 {
     public string Name { get; private set; }
+    public int HP { get; private set; }
+
+    public Enemy(string name)
+    {
+        Name = name;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
