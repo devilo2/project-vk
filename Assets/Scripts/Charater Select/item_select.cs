acd7106086b3a59ff5862f5b74115d1be44ec1af
PC-053\Administrator <kes2178@naver.com>
2024-12-02 02:58:34 +0900

    character select(탐험 창으로 넘어가는거 까지 성공)

--- Assets/Scripts/Charater Select/item_select.cs
+++ Assets/Scripts/Charater Select/item_select.cs
@@ -14,38 +14,84 @@ public class item_select : MonoBehaviour
     public Button next_select_btn;
 
     // Start is called before the first frame update
-    public void itemselect()
+    public void press_enable_energy()
     {
-        if(selected_item_num == MAX_ITEM_NUM)
+        if (selected_item_num == MAX_ITEM_NUM)
         {
-            for(int i =0; i < item.Length; i++)
+            for (int i = 0; i < item.Length; i++)
             {
                 item[i].GetComponent<Button>().interactable = false;
             }
             next_select_btn.interactable = true;
         }
-    }
-    
-    public void press_enable_energy()
-    {
-        selected_item_num++;
-        PlayerData.EnableEnergy += 1;
-        print("Enable_Energy"+ selected_item_num);
+        else
+        {
+            selected_item_num++;
+            PlayerData.EnableEnergy += 1;
+            print("Enable_Energy" + selected_item_num);
+        }
+        
     }
     public void press_returning_gear()
     {
-        selected_item_num++;
-        PlayerData.ReturningGear += 1;
-        print("Returning_Gear"+ selected_item_num);
+        if (selected_item_num == MAX_ITEM_NUM)
+        {
+            for (int i = 0; i < item.Length; i++)
+            {
+                item[i].GetComponent<Button>().interactable = false;
+            }
+            next_select_btn.interactable = true;
+        }
+        else
+        {
+            selected_item_num++;
+            PlayerData.ReturningGear += 1;
+            print("Returning_Gear" + selected_item_num);
+        }
     }
     public void press_self_recovery_power_capsule()
     {
-        selected_item_num++;
-        PlayerData.SelfRecoveryPowerCapsule += 1;
-        print("Self_Recovery_Power_Capsule"+ selected_item_num);
+        if (selected_item_num == MAX_ITEM_NUM)
+        {
+            for (int i = 0; i < item.Length; i++)
+            {
+                item[i].GetComponent<Button>().interactable = false;
+            }
+            next_select_btn.interactable = true;
+        }
+        else
+        {
+            selected_item_num++;
+            PlayerData.SelfRecoveryPowerCapsule += 1;
+            print("Self_Recovery_Power_Capsule" + selected_item_num);
+        }
     }
     void Start()
     {
+        playerData = GameObject.Find("PlayerManager").GetComponent<PlayerData>();
+        if (playerData == null)
+        {
+            Debug.LogError("PlayerManager not found or PlayerData component missing.");
+        }
+
+        item = GameObject.FindGameObjectsWithTag("item");
+        if (item.Length == 0)
+        {
+            Debug.LogError("No items found with the 'item' tag.");
+        }
+
+        select_item = GameObject.FindGameObjectsWithTag("item_select");
+        if (select_item.Length == 0)
+        {
+            Debug.LogError("No items found with the 'item_select' tag.");
+        }
+
+        next_select_btn = GameObject.Find("NextSelectButton").GetComponent<Button>();
+        if (next_select_btn == null)
+        {
+            Debug.LogError("Next select button not found.");
+        }
+        playerData = GameObject.Find("PlayerManager").GetComponent<PlayerData>();
         GameObject item_tab = GameObject.Find("Canvas").transform.Find("Tool").gameObject;
         item_tab.SetActive(true);
         item = GameObject.FindGameObjectsWithTag("item");
@@ -53,6 +99,7 @@ public class item_select : MonoBehaviour
         next_select_btn.interactable = false;
         for (int i = 0; i < item.Length; i++)
         {
+            item[i].GetComponent<Button>();
             item[i].SetActive(false);
         }
         item_tab.SetActive(false);
